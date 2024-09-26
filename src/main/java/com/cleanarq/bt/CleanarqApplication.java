125f9b07c9ccf1ad4ad15821fdf51d396b1cf611
kaianl <kaianlucianoalvesdossantos@gmail.com>
2024-09-26 09:11:55 -0300

    Init Commit

---
+++ src/main/java/com/cleanarq/bt/CleanarqApplication.java
@@ -0,0 +1,13 @@
+package com.cleanarq.bt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleanarq/bt/CleanarqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanarq.bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
